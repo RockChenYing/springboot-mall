4480dabb47b30d9b62e9efc2eb294f8c6e75dea6
tkbadmin <tkb0005432@gmail.com>
2025-01-03 17:43:20 +0800

    「商品功能-單元測試(ProductControllerTest)」

---
+++ src/test/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO product (product_name, category, image_url, price, stock, description, created_date, last_modified_date)
+VALUES ('蘋果 (澳洲)', 'FOOD', 'https://cdn.pixabay.com/photo/2016/11/30/15/00/apples-1872997_1280.jpg', 30, 10, '這是來自澳洲的蘋果！', '2022-03-19 17:00:00', '2022-03-22 18:00:00');
+
+INSERT INTO product (product_name, category, image_url, price, stock, description, created_date, last_modified_date)
+VALUES ('蘋果 (日本北海道)', 'FOOD', 'https://cdn.pixabay.com/photo/2017/06/13/42/apple-2788662_1280.jpg', 300, 5, '這是來自日本北海道的蘋果！', '2022-03-19 18:30:00', '2022-03-19 18:30:00');
+
+INSERT INTO product (product_name, category, image_url, price, stock, description, created_date, last_modified_date)
+VALUES ('好吃又甜的蘋果椪子', 'FOOD', 'https://cdn.pixabay.com/photo/2021/07/03/04/17/orange-6508617_1280.jpg', 10, 50, NULL, '2022-03-20 10:50:00', '2022-03-24 14:50:00');
+
+INSERT INTO product (product_name, category, image_url, price, stock, description, created_date, last_modified_date)
+VALUES ('Toyota', 'CAR', 'https://cdn.pixabay.com/photo/2014/05/18/19/13/toyota-347288_1280.jpg', 100000, 5, NULL, '2022-03-20 09:20:00', '2022-03-20 09:20:00');
+
+INSERT INTO product (product_name, category, image_url, price, stock, description, created_date, last_modified_date)
+VALUES ('BMW', 'CAR', 'https://cdn.pixabay.com/photo/2018/02/21/03/15/bmw-m4-3169357_1280.jpg', 500000, 3, '溫馨提醒：運動模式可切換至 DOHC 雙渦輪 + 16氣門', '2022-03-20 12:30:00', '2022-03-20 12:30:00');
+
+INSERT INTO product (product_name, category, image_url, price, stock, description, created_date, last_modified_date)
+VALUES ('Benz', 'CAR', 'https://cdn.pixabay.com/photo/2017/03/27/14/44/car-2179272_1280.jpg', 600000, 2, NULL, '2022-03-21 10:50:00', '2022-03-21 10:50:00');
+
+INSERT INTO product (product_name, category, image_url, price, stock, description, created_date, last_modified_date)
+VALUES ('Tesla', 'CAR', 'https://cdn.pixabay.com/photo/2021/11/15/16/49/tesla-5919764_1280.jpg', 450000, 5, '世界最酷的無污染汽車！', '2022-03-21 23:30:00', '2022-03-21 23:30:00');
