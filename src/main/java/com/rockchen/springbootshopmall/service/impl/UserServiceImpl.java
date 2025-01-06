126bf96c235db7dccaa9192fc5003ef0a8301cc1
tkbadmin <tkb0005432@gmail.com>
2025-01-06 16:59:09 +0800

    帳號功能- 使用 MD5 加密密碼

--- src/main/java/com/rockchen/springbootshopmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/rockchen/springbootshopmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -37,6 +38,10 @@ public class UserServiceImpl implements UserService {
             log.warn("該電子信箱 {} 已經被註冊", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         // 創建帳號
         return userDao.createUser(userRegisterRequest);
     }
@@ -44,14 +49,20 @@ public class UserServiceImpl implements UserService {
     @Override
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
+        // 檢查user是否存在
 
-        // 尚未註冊
+
+        // 檢查 user 是否存在
         if(user == null){
             log.warn("此信箱 {} 還不存在，尚未註冊" ,userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
         // 密碼檢驗
-        if(user.getPassword().equals(userLoginRequest.getPassword())){
+        if(user.getPassword().equals(hashedPassword)){
             return user;
         }else{
             log.warn("email {} 的密碼錯誤，請重新輸入:",userLoginRequest.getEmail());
